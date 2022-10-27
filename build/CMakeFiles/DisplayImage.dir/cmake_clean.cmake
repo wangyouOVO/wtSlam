27235924fe51d93fc268c3407acbda2ceee9c4f1
wenty <2216172764@qq.com>
2022-10-27 20:10:15 +0800

    init

---
+++ build/CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DisplayImage.dir/src/showVideo.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/showVideo.cpp.o.d"
+  "DisplayImage"
+  "DisplayImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
